65ff53ef6bc44ef9aa3ca7b1157002de13251267
Brad Hughes <bradleythughes@gmail.com>
2016-06-15 17:16:08 -0400

    Created Todo routes and controller logic.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var todosRouter = require('./routes/todos');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,7 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
+app.use('/todos', todosRouter);
 app.use('/', routes);
 app.use('/users', users);
 
